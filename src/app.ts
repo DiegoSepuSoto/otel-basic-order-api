0e29fee9cd588c785bd756bb2625ace43dea95a4
Diego Sepúlveda <diegosepu.soto@gmail.com>
2023-09-17 18:23:46 -0300

    improving instrumentation

--- src/app.ts
+++ src/app.ts
@@ -3,20 +3,23 @@ import axios from "axios";
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/order', async (req, res) => {
     const orderID = req.query.orderID;
 
     try {
-        const {data} = await axios.get(`${process.env.CUSTOMER_API_HOST}/api/customer?customerID=abc1234`);
         const delay = Math.floor(Math.random() * 1000) + 1000;
 
-        setTimeout(() => {
-            res.json({
-                orderID,
-                orderStatus: 'IN TRANSIT',
-                customer: data
-            });
-        }, delay)
+        setTimeout(() => {}, delay)
+
+        const {data} = await axios.get(`${process.env.CUSTOMER_API_HOST}/api/customer?customerID=abc1234`);
+
+        res.json({
+            orderID,
+            orderStatus: 'IN TRANSIT',
+            customer: data
+        });
     } catch (error) {
         res.status(500).send('an error occurred while making the API request :(');
     }
--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,14 +1,9 @@
-import * as opentelemetry from "@opentelemetry/sdk-node";
+import { NodeSDK } from "@opentelemetry/sdk-node";
 import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-grpc';
-import { OTLPMetricExporter } from "@opentelemetry/exporter-metrics-otlp-grpc";
-import { PeriodicExportingMetricReader } from "@opentelemetry/sdk-metrics";
 
-const sdk = new opentelemetry.NodeSDK({
+const sdk = new NodeSDK({
     traceExporter: new OTLPTraceExporter(),
-    metricReader: new PeriodicExportingMetricReader({
-        exporter: new OTLPMetricExporter(),
-    }),
     instrumentations: [new HttpInstrumentation()],
 });
 
